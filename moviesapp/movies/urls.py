3643bc5827a5f3026d634103d6e4db9b36df74ee
KamilFerenc <kamilferenc91@gmail.com>
2019-05-23 23:28:20 +0200

    Add movies.urls file, extend moviesapp.urls

---
+++ moviesapp/movies/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^movies/$', views.MoviesList.as_view(), name='movies_list'),
+    url(r'^movies/(?P<pk>\d+)/$',
+        views.MovieDetail.as_view(), name='movie_detail'),
+    ]
