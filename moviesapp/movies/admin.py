8d290fc0813129c72a25aff751cb912a6c45c0d4
KamilFerenc <kamilferenc91@gmail.com>
2019-05-24 09:01:33 +0200

    Add models to admin panel

--- moviesapp/movies/admin.py
+++ moviesapp/movies/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Comment, Movie
 
-# Register your models here.
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['user', 'body', 'created', 'movie']
+    list_filter = ['movie', 'created']
+
+
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ['Title', 'Year']
+    list_filter = ['Year', 'Genre']
+
+
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(Movie, MovieAdmin)
