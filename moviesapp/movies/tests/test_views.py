14413bcbc8737ccfa9265b2e904f5b3b9d7feacd
KamilFerenc <kamilferenc91@gmail.com>
2019-05-28 11:50:25 +0200

    Refactor test_views.py

--- moviesapp/movies/tests/test_views.py
+++ moviesapp/movies/tests/test_views.py
@@ -2,6 +2,7 @@ import pytest
 from django.urls import reverse
 from mixer.backend.django import mixer
 from rest_framework.test import APIRequestFactory, APITestCase
+
 from movies import views
 from movies import models
 
@@ -34,14 +35,16 @@ class TestMoviesList(APITestCase):
     # Test post request - different conditions
     def test_MovieList_post_valid_request(self):
         # Check if model exists in DB before request
-        check_model_in_db = \
+        check_model_in_db = (
             models.Movie.objects.filter(Title=self.title).exists()
+        )
         assert check_model_in_db is False
 
         req = APIRequestFactory().post(self.url, {'Title': self.title})
         resp = views.MoviesList.as_view()(req)
-        check_model_in_db = \
+        check_model_in_db = (
             models.Movie.objects.filter(Title=self.title).exists()
+        )
         assert resp.status_code == 201
         assert resp.data['Title'] == self.title
         # Check if model exists in DB after request
@@ -62,18 +65,20 @@ class TestMoviesList(APITestCase):
             self.url, {'Title': self.movie_1.Title})
         resp = views.MoviesList.as_view()(req)
         expected_response = {
-            'Warning': '{} already exists in database.'.format(self.movie_1.Title)
+            'Warning': (
+                '{} already exists in database.'.format(self.movie_1.Title)
+            )
         }
         assert resp.status_code == 204
         assert resp.data == expected_response
 
-    # The request for movie which is not present in omdb api
+    # The request for movie which is not present in OMDb api
     def test_MovieList_post_movie_not_exists_omdb(self):
         req = APIRequestFactory().post(self.url,
                                        {'Title': '!!realystranegetitlemovie!!'})
         resp = views.MoviesList.as_view()(req)
         expected_response = {
-            'Error': "Movie with that title hasn't been found."
+            'Error': 'Movie with that title has not been found.'
         }
         assert resp.status_code == 204
         assert resp.data == expected_response
@@ -82,10 +87,10 @@ class TestMoviesList(APITestCase):
     def test_MovieList_get(self):
         req = APIRequestFactory().get(self.url)
         resp = views.MoviesList.as_view()(req)
-
-        assert resp.data['count'] == 2, \
-            'Should return 2 because two objects have been create in setUp and' \
-            'already exists in DB'
+        assert resp.data['count'] == 2, (
+            'Should return 2 because two objects have been create in setUp and '
+            'already exists in DB.'
+        )
         assert resp.status_code == 200
 
 
@@ -96,7 +101,7 @@ class TestCommentsList(APITestCase):
         self.comment_data = {
             'user': 'Test User',
             'comment': 'Test comment description.',
-            'movie': self.movie.pk
+            'movie': self.movie.pk,
         }
         self.url = reverse('movies:comments-list')
 
@@ -105,15 +110,17 @@ class TestCommentsList(APITestCase):
         user = self.comment_data['user']
         comment = self.comment_data['comment']
         movie = self.comment_data['movie']
-        check_comment_in_db = \
+        check_comment_in_db = (
             models.Comment.objects.filter(user=user, comment=comment).exists()
+        )
         assert check_comment_in_db is False
 
         req = APIRequestFactory().post(self.url, self.comment_data)
         resp = views.CommentsList.as_view()(req)
         # Check if comment exists in DB after request
-        check_comment_in_db = \
+        check_comment_in_db = (
             models.Comment.objects.filter(user=user, comment=comment).exists()
+        )
         assert user and movie and comment in resp.data.values()
         assert resp.status_code == 201
         assert check_comment_in_db is True
@@ -121,32 +128,35 @@ class TestCommentsList(APITestCase):
     def test_CommentList_post_invalid_data(self):
         invalid_data = {**self.comment_data,
                         'comment': '',
-                        'user': ''}
+                        'user': '',
+                        }
         user = self.comment_data['user']
         comment = self.comment_data['comment']
-        movie = self.comment_data['movie']
         # Check if comment exists in DB before request
-        check_comment_in_db = \
+        check_comment_in_db = (
             models.Comment.objects.filter(user=user, comment=comment).exists()
+        )
         assert check_comment_in_db is False
 
         req = APIRequestFactory().post(self.url, invalid_data)
         resp = views.CommentsList.as_view()(req)
 
         # Check if comment exists in DB after request
-        check_comment_in_db = \
+        check_comment_in_db = (
             models.Comment.objects.filter(user=user, comment=comment).exists()
+        )
         assert resp.status_code == 400
         assert check_comment_in_db is False
 
     def test_CommentList_get(self):
-        # Create 5 comment object and save in DB
+        # Create 5 comments object and save in DB
         mixer.cycle(5).blend('movies.Comment', movie=self.movie)
         req = APIRequestFactory().get(self.url)
         resp = views.CommentsList.as_view()(req)
         assert resp.status_code == 200
-        assert resp.data['count'] == 5, \
-            'Should return 5 - 5 objects have been created (mixer.cycle(5)'
+        assert resp.data['count'] == 5, (
+            'Should return 5 - 5 objects have been created (mixer.cycle(5)).'
+        )
 
 
 class TestTopList(APITestCase):
@@ -172,43 +182,54 @@ class TestTopList(APITestCase):
 
         self.url = reverse('movies:top')
 
-   # Test functionality without passing parameters 'since', 'to'
+    # Test functionality without passing parameters 'since', 'to'
     def test_TopList_default_date(self):
         req = APIRequestFactory().get(self.url)
         resp = views.TopList.as_view()(req)
         # data - list of OrdereDict - total_comments ordered descending
         data = resp.data
 
-        rank_position_1 = data[0]['id'], data[0]['rank'], data[0]['total_comments']
-        assert rank_position_1 == (1, 1, 3), \
-            'Based on the setUp parameters test should return movie with:' \
-            ' id=1, at rank position = 1 and 3 comments'
+        rank_position_1 = (
+            data[0]['id'], data[0]['rank'], data[0]['total_comments']
+        )
+        assert rank_position_1 == (1, 1, 3), (
+            'Based on the setUp parameters test should return movie with: '
+            'id=1, at rank position = 1 and 3 comments.'
+        )
         assert data[1]['rank'] and data[2]['rank'] == 2
 
         # Test movies with rank position = 2
-        assert self.movie_5.id and self.movie_3.id in (data[1]['id'], data[2]['id'])
-        assert data[1]['rank'] and data[2]['rank'] == 2, \
-            'Based on the setUp parameters test should return two movies with' \
-            ' the same rank.'
-        assert data[1]['total_comments'] and data[2]['total_comments'] == 2,\
-            'Based on the setUp parameters test should return two movies with' \
-            ' the same total_comments.'
+        assert (
+                self.movie_5.id and self.movie_3.id
+                in (data[1]['id'], data[2]['id'])
+        )
+        assert data[1]['rank'] and data[2]['rank'] == 2, (
+            'Based on the setUp parameters test should return two movies with '
+            'the same rank.'
+        )
+        assert data[1]['total_comments'] and data[2]['total_comments'] == 2, (
+            'Based on the setUp parameters test should return two movies with '
+            'the same total_comments.'
+        )
 
         # Test movies with rank position = 4
         assert self.movie_2.id and self.movie_4.id in (data[3]['id'], data[4]['id'])
-        assert data[3]['rank'] and data[4]['rank'] == 4, \
-            'Based on the setUp parameters test should return two movies with' \
-            ' the same rank.'
-        assert data[3]['total_comments'] and data[4]['total_comments'] == 1, \
-            'Based on the setUp parameters test should return two movies with' \
-            ' the same total_comments.'
+        assert data[3]['rank'] and data[4]['rank'] == 4, (
+            'Based on the setUp parameters test should return two movies with '
+            'the same rank.'
+        )
+        assert data[3]['total_comments'] and data[4]['total_comments'] == 1, (
+            'Based on the setUp parameters test should return two movies with'
+            'the same total_comments.'
+        )
 
         # Test movies with rank position = 6
         assert self.movie_6.id == data[5]['id']
         assert data[5]['rank'] == 6
-        assert data[5]['total_comments'] == 0, \
-            'Based on the setUp parameters test should return movie with' \
+        assert data[5]['total_comments'] == 0, (
+            'Based on the setUp parameters test should return movie with'
             'total_comments without any comments.'
+        )
         assert resp.status_code == 200
 
     # No comments were created in the requested date range
@@ -218,16 +239,17 @@ class TestTopList(APITestCase):
         resp = views.TopList.as_view()(req)
         # data - list of OrdereDict - total_comments ordered descending
         data = resp.data
-        expected_id = [
+        expected_id = (
             self.movie_1.id, self.movie_2.id, self.movie_3.id,
             self.movie_4.id, self.movie_5.id, self.movie_6.id,
-        ]
+        )
         for i in data:
             assert i['rank'] == 1
             assert i['total_comments'] == 0
             assert i['id'] in expected_id
-        assert len(data) == 6, 'Should return 6 objects, ' \
-                               'because 6 movies already exists in DB'
+        assert len(data) == 6, (
+            'Should return 6 objects, because 6 movies already exists in DB.'
+        )
         assert resp.status_code == 200
 
     # Test functionality without 'since' parameter
@@ -235,8 +257,9 @@ class TestTopList(APITestCase):
         to = '2005-05-01'
         req = APIRequestFactory().get(self.url, {'to': to})
         resp = views.TopList.as_view()(req)
-        assert len(resp.data) == 6, 'Should return 6 objects,' \
-                                    'because 6 movies already exists in DB'
+        assert len(resp.data) == 6, (
+            'Should return 6 objects, because 6 movies already exists in DB.'
+        )
         assert resp.status_code == 200
 
     # Test functionality without 'to' parameter
@@ -244,8 +267,9 @@ class TestTopList(APITestCase):
         since = '2012-08-11'
         req = APIRequestFactory().get(self.url, {'since': since})
         resp = views.TopList.as_view()(req)
-        assert len(resp.data) == 6, 'Should return 6 objects,' \
-                                    'because 6 movies already exists in DB'
+        assert len(resp.data) == 6, (
+            'Should return 6 objects, because 6 movies already exists in DB.'
+        )
         assert resp.status_code == 200
 
     # Date in incorrect format
@@ -255,7 +279,10 @@ class TestTopList(APITestCase):
         req = APIRequestFactory().get(self.url, {'since': since,
                                                  'to': '2005-05-02'})
         resp = views.TopList.as_view()(req)
-        expected_resp = {'Error': 'Incorrect date format - {}'.format(since)}
+        expected_resp = (
+            {'Error': 'Incorrect date format - {}, please enter a date in '
+                      'format YYYY-M-D (e.g 2015-04-23)'.format(since)}
+        )
         assert resp.data == expected_resp
         assert resp.status_code == 400
 
@@ -265,6 +292,9 @@ class TestTopList(APITestCase):
         req = APIRequestFactory().get(self.url, {'since': '2005-05-02',
                                                  'to': to})
         resp = views.TopList.as_view()(req)
-        expected_resp = {'Error': 'Incorrect date format - {}'.format(to)}
+        expected_resp = (
+            {'Error': 'Incorrect date format - {}, please enter a date in '
+                      'format YYYY-M-D (e.g 2015-04-23)'.format(to)}
+        )
         assert resp.data == expected_resp
         assert resp.status_code == 400
